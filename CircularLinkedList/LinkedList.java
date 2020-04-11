481f04c24cb4e2e1b056c69a8f093b15ad783207
lrak32124 <lrak32124@gmail.com>
2020-04-11 12:10:40 +0800

    Isa pa

--- CircularLinkedList/LinkedList.java
+++ CircularLinkedList/LinkedList.java
@@ -5,16 +5,56 @@ import java.util.List;
 
 public class LinkedList {
 
+    Node head;
 
-    Node first = new Node();
-    Node second = new Node();
-    Node third = new Node();
-    Node fourth = new Node();
-    Node fifth = new Node();
+    void init() {
+        Node node1 = new Node();
+        Node node2 = new Node();
+        Node node3 = new Node();
+        Node node4 = new Node();
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node1;
+        head = node1;
+    }
+
+    public void insert(int data) {
+        Node node = new Node();
+        node.data = data;
+
+        Node last = head;
 
 
+        while (last.next != head) {
+            last = last.next;
+        }
+        last.next = node;
+        node.next = head;
+        head = node;
 
-    public void insert(int data){
+    }
+
+    public void delete() {
+        Node node = head;
+        while (node.next != head) {
+            node = node.next;
+        }
+        Node last = node;
+        Node first = last.next;
+        last.next = first.next;
+        first = first.next;
+        head = first;
+
+    }
+
+    public void print() {
+        Node node = head;
+        while (node.next != head) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.print(node.data);// printing last node
 
     }
 
--- CircularLinkedList/Runner.java
+++ CircularLinkedList/Runner.java
@@ -1,4 +1,18 @@
 package CircularLinkedList;
 
 public class Runner {
+
+    public static void main (String args[]){
+       LinkedList linkedList = new LinkedList();
+
+       linkedList.init();
+       linkedList.insert(5);
+
+
+       linkedList.print();
+        linkedList.insert(100);
+        linkedList.print();
+
+    }
+
 }
